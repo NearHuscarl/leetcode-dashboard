b3d849d095bc82b475fa8ed7a8387227003831f7
Near Huscarl <near.huscarl@gmail.com>
2023-05-16 11:02:37 +0700

    fetch all LC problems

--- src/app/api/leetcode.ts
+++ src/app/api/leetcode.ts
@@ -19,10 +19,29 @@ export const useLeetcodeProblems = () => {
   return useQuery<unknown, unknown, Record<string, TLeetcode>>(
     ["leetcodeProblems"],
     async () => {
-      // TODO: fetch all LC problems (limit=3000) leads to timeout error (free quota 10 seconds per request)
-      const response = await fetch(`${BASE_URL}/api/leetcode?limit=1000`);
-      const data = await response.json();
-      return data.problems;
+      const fetchProblems = (skip: number) =>
+        fetch(`${BASE_URL}/api/leetcode?limit=500&skip=${skip}`).then((r) =>
+          r.json()
+        );
+      // only fetch 500 problems at a time to get around timeout error (free quota 10 seconds per request)
+      const all = (await Promise.all([
+        fetchProblems(0),
+        fetchProblems(500),
+        fetchProblems(1000),
+        fetchProblems(1500),
+        fetchProblems(2000),
+        fetchProblems(2500),
+        fetchProblems(3000),
+      ])) as { problems: Record<string, TLeetcode> }[];
+
+      const problems: Record<string, TLeetcode> = {};
+      for (const data of all) {
+        for (const id in data.problems) {
+          problems[id] = data.problems[id];
+        }
+      }
+
+      return problems;
     },
     {
       staleTime: 1000 * 60 * 60 * 24 * 7, // 1 week
